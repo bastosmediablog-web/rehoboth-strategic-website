bce46b53719a25788e1b93eee7ea0db5eca77581
bastosmediablog-web <bastosmediablog@gmail.com>
2026-07-18 21:50:22 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,93 @@
+const container = document.getElementById("product-container");
+
+
+function displayProducts(items){
+
+container.innerHTML="";
+
+
+items.forEach(product=>{
+
+
+let stockStatus = product.stock 
+? "✅ In Stock" 
+: "❌ Out of Stock";
+
+
+let button = product.stock
+? `<button onclick="addToCart(${product.id})">
+Add To Cart
+</button>`
+:
+`<button disabled>
+Out of Stock
+</button>`;
+
+
+container.innerHTML += `
+
+
+<div class="product-card">
+
+
+<img src="${product.image}" alt="${product.name}">
+
+
+<h3>${product.name}</h3>
+
+
+<p>${product.description}</p>
+
+
+<p>
+<strong>Category:</strong> ${product.category}
+</p>
+
+
+<p>
+<strong>${stockStatus}</strong>
+</p>
+
+
+<h4>
+${product.price}
+</h4>
+
+
+${button}
+
+
+</div>
+
+
+`;
+
+
+});
+
+
+}
+
+
+
+displayProducts(products);
+
+
+
+function filterProducts(category){
+
+if(category=="All"){
+
+displayProducts(products);
+
+}
+
+else{
+
+let filtered = products.filter(product=>product.category == category);
+
+displayProducts(filtered);
+
+}
+
+}
